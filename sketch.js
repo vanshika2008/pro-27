c38d8bcc10272cb014e783b64ed19388351548f4
vanshika2008 <65905731+vanshika2008@users.noreply.github.com>
2020-11-30 19:06:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -52,7 +52,7 @@ function draw(){
 
 function keyPressed(){
     if(keyCode === 38 ){
-       Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-770,y:70});
+       Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-770,y:-70});
        
     }
   }
